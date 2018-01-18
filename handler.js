dcc190bb346bcb154050795363d5381ab23585ac
Igor Marcelo de Oliveira da Silva <cccmigor@gmail.com>
2018-01-18 00:28:39 -0200

    correções

--- handler.js
+++ handler.js
@@ -6,47 +6,48 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const s3 = new AWS.S3();
 
 module.exports.extractMetadata = (event, context, callback) => {
-    const key = event.Records[0].s3.object.key;
-    const bucket = event.Records[0].s3.bucket.name;
 
     let tableParams = {
-        TableName: 'serverless-challenge-dev',
+        TableName: process.env.DYNAMODB_TABLE,
         Item: {
-            s3objectkey: '04',
-            content_length: 40,
+            s3objectkey: '',
+            content_length: 0,
             height: 0,
             width: 0
         }
     };
 
-    let s3Params = {
-        Bucket: bucket, 
-        Key: key
-    };
+    for (var rec in event.Records) {
+        const key = event.Records[rec].s3.object.key;
+        const bucket = event.Records[rec].s3.bucket.name;
 
-    s3.getObject(s3Params, function(err, data) {
-        if (err) {
-            callback(err, null);
-        } // an error occurred
-        else {
-            const parser = exif.create(data.Body);
-            const result = parser.parse();
-            tableParams.Item.s3objectkey = key;
-            tableParams.Item.content_length = data.ContentLength;
-            tableParams.Item.width = result.imageSize.width;
-            tableParams.Item.height = result.imageSize.height;
+        let s3Params = {
+            Bucket: bucket, 
+            Key: key
+        };
 
-            dynamoDb.put(tableParams, function (err, data) {
-                if (err) {
-                    console.log(err);
-                    callback(err, null);
-                }
-                else {
-                    callback(null, { 'message': "Success" });
-                }
-            });
-        } // successful response
-    });
+        s3.getObject(s3Params, function(err, data) {
+            if (err) {
+                console.log(err);
+                callback(err, null);
+            } // an error occurred
+            else {
+                const parser = exif.create(data.Body);
+                const result = parser.parse();
+                tableParams.Item.s3objectkey = key;
+                tableParams.Item.content_length = data.ContentLength;
+                tableParams.Item.width = result.imageSize.width;
+                tableParams.Item.height = result.imageSize.height;
+    
+                dynamoDb.put(tableParams, function (err, data) {
+                    if (err) {
+                        console.log(err);
+                        callback(err, null);
+                    }
+                });
+            } // successful response
+        });
+    }
 };
 
 module.exports.getMetadata = (event, context, callback) => {  
@@ -54,7 +55,7 @@ module.exports.getMetadata = (event, context, callback) => {
     const s3objectkey = event.path.replace(path, '')
 
     let params = {
-        TableName: 'serverless-challenge-dev',
+        TableName: process.env.DYNAMODB_TABLE,
         Key: {
             s3objectkey: s3objectkey
         }
